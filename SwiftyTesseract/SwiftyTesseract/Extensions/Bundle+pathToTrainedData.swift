f8e8d133839052abc9b303b0d7a8bb43f689de6d
Steven Sherry <steven.r.sherry@gmail.com>
2018-10-23 22:10:45 -0500

    Fix: Changed implementation of `pathToTrainedData` to use URLs path property instead of absoluteString
    Using URL.absolutePath caused targets using SwiftyTesseract that had spaces in their names
    to have percent encoding inserted into the URL. This would cause the underlying Tesseract
    library to error because it could not find /path/to/Target With Spaces.app because it was
    looking for /path/to/Target%20With%20Spaces.app instead.

--- SwiftyTesseract/SwiftyTesseract/Extensions/Bundle+pathToTrainedData.swift
+++ SwiftyTesseract/SwiftyTesseract/Extensions/Bundle+pathToTrainedData.swift
@@ -10,12 +10,6 @@ import Foundation
 
 extension Bundle {
   var pathToTrainedData: String {
-    // Including the file:// prefix will cause TessBaseAPIInit to fail
-    let intermediatePath = self.bundleURL.appendingPathComponent("tessdata").absoluteString
-    let pathPrefix = "file://"
-    let pathPrefixEnd = String.Index(encodedOffset: pathPrefix.count)
-    let trimmedPath = intermediatePath[pathPrefixEnd...]
-    guard let finalPath = trimmedPath.removingPercentEncoding else { fatalError("") }
-    return String(finalPath)
+    return bundleURL.appendingPathComponent("tessdata").path
   }
 }
